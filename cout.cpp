0c35f2624dcc828900008a8598d6ca9ddf5a51da
Shayan Dodge <94797491+ShayanDodge@users.noreply.github.com>
2022-05-19 15:35:59 +0430

    Delete cout.cpp

--- cout.cpp
+++
@@ -1,9 +0,0 @@
-ijjj// A Small C++ Program
-# include <iostream>
-# include <conio.h>
-using namespace std;
-main()
-{
-	cout<<"Hello World"<<endl;// endl: ends the current line of output
-	getch();// don't show time in the results
-}
