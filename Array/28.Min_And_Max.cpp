72e1dbf600591069ce858e94732d669a32445e51
Shayan Dodge <94797491+ShayanDodge@users.noreply.github.com>
2022-05-24 19:32:49 +0430

    Add files via upload

---
+++ Array/28.Min_And_Max.cpp
@@ -0,0 +1,34 @@
+\\ Find the min and max and average of 5 integers that a user will input.
+
+#include <iostream>
+
+int main()
+{
+    int userInput = 0;
+    int maxNumber = 0;
+    int minNumber = 100;
+    int sumTotal = 0;
+    float average = 0;
+
+    //get the numbers from the user
+    for(int i = 0; i < 5; i++)
+    {
+        std::cout << "Enter a number: ";
+        std::cin>>userInput;
+        std::cout << userInput << "\n";
+        if(userInput > maxNumber)
+        {
+            maxNumber = userInput;
+        }
+        if(userInput < minNumber)
+        {
+            minNumber = userInput;
+        }
+        sumTotal = sumTotal + userInput;
+    }
+    std::cout << "Maximum number = " << maxNumber << "\n";
+    std::cout << "Minimum number = " << minNumber << "\n";
+    average = sumTotal / 15.0;
+    std::cout << "Average = " << average << "\n";
+    return 0;
+}
