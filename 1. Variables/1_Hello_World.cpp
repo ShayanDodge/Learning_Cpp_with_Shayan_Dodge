c0ebe3b52d6b9c6740991fce120aed52c2e7f911
Shayan Dodge <dodgeshayan@gmail.com>
2023-03-09 09:53:33 +0330

    var

--- 1. Variables/1_Hello_World.cpp
+++ 1. Variables/1_Hello_World.cpp
@@ -1,10 +1,12 @@
 // c++ hello world program
 
 #include <iostream>
+#include<conio.h>
 using namespace std;
 
 int main() {
          // Program code here
          cout << "Hello world!";
+         getch();
          return 0;
 }
--- 1. Variables/2_Add_Two_Integers.cpp
+++ 1. Variables/2_Add_Two_Integers.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
 int main() {
@@ -10,5 +11,6 @@ int main() {
   cin >> b;
   sum = a + b;
   cout << a << " + " <<  b << " = " << sum;     
+  getch();
   return 0;
 }
